0d7e5384c93bfa2bf3a90ddae6a11b3fdf492e32
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-12-10 14:25:24 +0100

    Update INTERCAMBIO_PARTICION.sql

--- INTERCAMBIO_PARTICION.sql
+++ INTERCAMBIO_PARTICION.sql
@@ -1,9 +1,10 @@
 /*
 Procedimiento AGR_INTERCAMBIO_PARTICION.
+Transfiere una partici√≥n, de una fecha determinada, de una tabla origen a otra destino.
 
-Par·metros: @v_source_tab_name. Nombre de la tabla cuya particiÛn se quiere transferir.
-			@v_target_tab_name. Nombre de la tabla a la que se quiere transferir la particiÛn.
-			@v_date. Fecha de la particiÛn a transferir (la fecha est· en el intervalo etl_particiones.left_boundary y etl_particiones.right_boundary).					
+Par√°metros: @v_source_tab_name. Nombre de la tabla cuya partici√≥n se quiere transferir.
+			@v_target_tab_name. Nombre de la tabla a la que se quiere transferir la partici√≥n.
+			@v_date. Fecha de la partici√≥n a transferir (la fecha est√° en el intervalo etl_particiones.left_boundary y etl_particiones.right_boundary).					
 					
 Incluye output de errores y eventos.			
 
@@ -20,7 +21,7 @@ BEGIN
 DECLARE @v_partition_number INT, @v_sql VARCHAR(MAX), @v_count INT, @v_tab_name VARCHAR(128)
 DECLARE cur_check_tabs CURSOR FOR SELECT @v_source_tab_name UNION SELECT @v_target_tab_name
 
---ComprobaciÛn de existencia de las tablas recibidas
+--Comprobaci√≥n de existencia de las tablas recibidas
 OPEN cur_check_tabs
 FETCH NEXT FROM cur_check_tabs INTO @v_tab_name
 WHILE @@FETCH_STATUS = 0
@@ -34,7 +35,7 @@ END
 CLOSE cur_check_tabs
 DEALLOCATE cur_check_tabs
 
---ObtenciÛn de ID de particiÛn
+--Obtenci√≥n de ID de partici√≥n
 BEGIN TRY 
 	SELECT @v_partition_number = partition_number
 	FROM (SELECT DISTINCT TOP (100) PERCENT f.name AS file_group_name, SCHEMA_NAME(t.schema_id) AS table_schema, p.partition_number AS partition_number, pf.boundary_value_on_right, 
@@ -53,13 +54,13 @@ BEGIN TRY
 	AND aux.table_schema = 'dbo'
 	
 	IF @@ROWCOUNT = 0
-		PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se encuentra particiÛn para la fecha y tabla indicadas'	
+		PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se encuentra partici√≥n para la fecha y tabla indicadas'	
 END TRY  
 BEGIN CATCH  
-	PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se ha podido ejecutar la b˙squeda de particiÛn'
+	PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se ha podido ejecutar la b√∫squeda de partici√≥n'
 END CATCH;
 
---ConstrucciÛn de sentencia de intercambio de particiÛn
+--Construcci√≥n de sentencia de intercambio de partici√≥n
 SET @v_sql = N'ALTER TABLE ' + '[dbo].' + @v_source_tab_name + ' SWITCH PARTITION ' + CONVERT(VARCHAR(100), @v_partition_number) + ' TO ' + '[dbo].' + @v_target_tab_name + ' PARTITION ' + CONVERT(VARCHAR(100), @v_partition_number)
 
 PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'INFO: Sentencia DDL ' + @v_sql
@@ -67,9 +68,9 @@ PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'INFO: Sentencia DDL
 --Intercambio
 BEGIN TRY  
 
-	--EjecuciÛn de DDL
+	--Ejecuci√≥n de DDL
 	EXEC(@v_sql)
-	--Check de operaciÛn realizada
+	--Check de operaci√≥n realizada
 	SELECT @v_count = COUNT(1)
 	FROM etl_particiones
 	WHERE name = @v_target_tab_name 
@@ -77,11 +78,11 @@ BEGIN TRY
 	AND partition_number = @v_partition_number
 
 	IF @v_count = 1
-		PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'INFO: Se ha transferido la particiÛn ' + CONVERT(VARCHAR(100), @v_partition_number) + ' a la tabla [dbo].' + @v_target_tab_name
+		PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'INFO: Se ha transferido la partici√≥n ' + CONVERT(VARCHAR(100), @v_partition_number) + ' a la tabla [dbo].' + @v_target_tab_name
 
 END TRY  
 BEGIN CATCH  
-	PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se ha podido ejecutar el intercambio de particiÛn'
+	PRINT CONVERT(VARCHAR(50), GETDATE()) + CHAR(9) + CHAR(9) + 'ERROR: No se ha podido ejecutar el intercambio de partici√≥n'
 END CATCH;
 
 END
